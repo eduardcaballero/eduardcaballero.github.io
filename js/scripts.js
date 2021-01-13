f2fe544f7c6ac45bf4b0c9d9e6c9b7dfefa443e6
eduardcaballero <eduardcaballerop@gmail.com>
2021-01-12 22:04:49 -0500

    act

--- js/scripts.js
+++ js/scripts.js
@@ -65,19 +65,4 @@
       });
     });
   
-  })(jQuery); // End of use strict
-  $(document).ready(function(){
-    $(".fancybox").fancybox({
-          openEffect: "none",
-          closeEffect: "none"
-      });
-      
-      $(".zoom").hover(function(){
-      
-      $(this).addClass('transition');
-    }, function(){
-          
-      $(this).removeClass('transition');
-    });
-  });
-      
+  })(jQuery); // End of use strict  
